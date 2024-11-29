287559214532ccdac13d47ee6be522636b4ac0b0
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-29 23:27:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import BookList from './components/BookList.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello! This is your Library:</h1>
+        <BookList/>
+       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
